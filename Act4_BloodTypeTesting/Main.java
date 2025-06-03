ba3c05965141def627050044cf652185927236f6
LD <ldaneliee33@gmail.com>
2025-06-03 18:56:56 +0800

    added try catch on Act4 Main.java

--- Act4_BloodTypeTesting/Main.java
+++ Act4_BloodTypeTesting/Main.java
@@ -8,53 +8,60 @@ public class Main {
         BloodType[] human = new BloodType[3];
 
         for (int i = 0; i < 3; i++) {
-            System.out.println("\n===== Blood Type Testing =====");
-            System.out.print("Enter Name: ");
-            String name = scan.next().trim();
-            System.out.print("Enter Age: ");
-            int age = Integer.parseInt(scan.next());
-            human[i] = new BloodType(name, age);
+            try{
+                System.out.println("\n===== Blood Type Testing =====");
+                System.out.print("Enter Name: ");
+                String name = scan.nextLine().trim();
+                System.out.print("Enter Age: ");
+                int age = Integer.parseInt(scan.nextLine());
+                human[i] = new BloodType(name, age);
 
-            System.out.println("\nChoose blood type input method:");
-            System.out.println("1. Full Type(e.g., A+)");
-            System.out.println("2. ABO and Rh separately(e.g., A and +)");
-            System.out.println("3. Numeric code (1 = A+, ..., 8 = AB-)");
-            System.out.print("Enter Choice: ");
-            int choice = Integer.parseInt(scan.next());
+                System.out.println("\nChoose blood type input method:");
+                System.out.println("1. Full Type(e.g., A+)");
+                System.out.println("2. ABO and Rh separately(e.g., A and +)");
+                System.out.println("3. Numeric code (1 = A+, ..., 8 = AB-)");
+                System.out.print("Enter Choice: ");
+                int choice = Integer.parseInt(scan.nextLine());
 
-            System.out.println("\n");
-            switch(choice){
+                System.out.println("\n");
+                switch(choice){
 
-                case 1:
+                    case 1:
 
-                    System.out.print("Enter full blood type (e.g., O-): ");
-                    human[i].processBloodType(scan.next().trim().toUpperCase());
-                    break;
+                        System.out.print("Enter full blood type (e.g., O-): ");
+                        human[i].processBloodType(scan.nextLine().trim().toUpperCase());
+                        break;
 
-                case 2:
+                    case 2:
 
-                    System.out.print("Enter ABO (A, B, AB, O): ");
-                    String ABO = scan.next().trim().toUpperCase();
+                        System.out.print("Enter ABO (A, B, AB, O): ");
+                        String ABO = scan.nextLine().trim().toUpperCase();
 
-                    System.out.print("Enter Rh (+ or -): ");
-                    String Rh = scan.next().trim();
-                    char charRh = Rh.charAt(0);
-                    human[i].processBloodType(ABO, charRh);
-                    break;
+                        System.out.print("Enter Rh (+ or -): ");
+                        String Rh = scan.nextLine().trim();
+                        char charRh = Rh.charAt(0);
+                        human[i].processBloodType(ABO, charRh);
+                        break;
 
-                case 3:
+                    case 3:
 
-                    System.out.print("Enter Numeric Code (e.g., 1): ");
-                    human[i].processBloodType(Integer.parseInt(scan.next()));
-                    break;
+                        System.out.print("Enter Numeric Code (e.g., 1): ");
+                        human[i].processBloodType(Integer.parseInt(scan.nextLine()));
+                        break;
 
-                default:
+                    default:
 
-                    System.out.println("Wrong Input");
-                    break;
+                        System.out.println("Wrong Input");
+                        break;
+                }
+                System.out.println();
+            } catch(NumberFormatException e){
+                System.err.println("Invalid number format! Try again!");
+                i--;
+                continue;
             }
-            System.out.println();
         } scan.close();
+        
 
         for(BloodType humans : human){
             humans.printDetails();
