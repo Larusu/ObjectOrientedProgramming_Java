7eda7896a79ee1979efd6f444ae13a8e3f95d686
LD <ldaneliee33@gmail.com>
2025-06-24 23:33:31 +0800

    updated

--- Act6_EmployeeRegistration/Employee.java
+++ Act6_EmployeeRegistration/Employee.java
@@ -2,6 +2,7 @@ package Act6_EmployeeRegistration;
 
 import javax.swing.JPanel;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 public class Employee extends Person implements Displayable{
@@ -12,15 +13,21 @@ public class Employee extends Person implements Displayable{
 
     @Override
     public void display(){
+
         JFrame frame = new JFrame();
-        frame.setTitle("Employee Registration");
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Panel panel = new Panel();
-        frame.add(panel);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        try{
+            
+            frame.setTitle("Employee Registration");
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Panel panel = new Panel();
+            frame.add(panel);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(frame, "Invalid Number Input!", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
     
--- Act6_EmployeeRegistration/Panel.java
+++ Act6_EmployeeRegistration/Panel.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -21,12 +22,14 @@ public class Panel extends JPanel {
     
     Employee employee = new Employee();
     JTextArea textArea;
-    ArrayList<JTextField> inputFields;
+    ArrayList<JComponent> inputFields;
 
     Panel(){
 
         this.setLayout(new BorderLayout());
         this.setPreferredSize(new Dimension(width, height));
+
+        inputFields = new ArrayList<>();
         this.add(bodyPanel(), BorderLayout.NORTH);
     }
 
@@ -40,23 +43,22 @@ public class Panel extends JPanel {
         JLabel label;
         String[] labelStrings = {"First Name", "Last Name", "Age", "Gender", "Email",
                                 "Contact No.", "Department", "Position", "Employee ID", "Salary"};
-
-        JTextField textField = new JTextField();
-        textField.setPreferredSize(new Dimension(width / 2, 30));
-
         String[] genderList = {"Male", "Female", "Non-Binary"};
-        JComboBox<String> gender  = new JComboBox<>(genderList);
-        gender.setPreferredSize(new Dimension(width / 2, 30));
-
+        
         for(String strings : labelStrings){
             label = new JLabel(strings + ":");
             label.setHorizontalAlignment(SwingConstants.LEFT);
             panel.add(label);
 
             if(strings.equals("Gender")){
+                JComboBox<String> gender  = new JComboBox<>(genderList);
+                gender.setPreferredSize(new Dimension(width / 2, 30));
+                inputFields.add(gender);
                 panel.add(gender);
                 continue;
             }
+            JTextField textField = new JTextField();
+            textField.setPreferredSize(new Dimension(width / 2, 30));
             inputFields.add(textField);
             panel.add(textField);
         }
